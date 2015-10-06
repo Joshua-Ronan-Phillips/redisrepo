b6cabfa152037f25bf7acb5bce1f3fdc37113c26
Conor C <Conorc1000@gmail.com>
2015-10-06 16:15:22 +0100

    http requests

--- server.js
+++ server.js
@@ -5,27 +5,27 @@ var client = redis.createClient();
 var frontend = fs.readFileSync(__dirname + '/public/frontend.js');
 var index = fs.readFileSync(__dirname + '/public/index.html');
 
-
-// set the port for the server
 var port = process.env.PORT || 8000;
 
 function handler(request, response) {
-    //display 'HELLO WORLD' when the user is on the home page
-    var url = request.url; //e.g. '/'
-    console.log(url);
+    var url = request.url;
+    var req = url.split('/');
+    var input = req[2];
+    console.log(input);
     if (url.length === 1) {
       response.writeHead(200, {"Content-Type": "text/html"});
       response.end(index);
     } else if (request.url.indexOf('/post') > -1) {
         console.log('post');
-        client.rpush("favourites", input, function(err, reply) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log(reply);
-            }
+        client.rpush("favourites", input, function(err, reply){
+          
         });
+        client.lrange('favourites', 0,-1, function(err, reply){
+          var list = reply.toString();
+          console.log(reply);
+        });
+
+
     }else if(request.url.indexOf('/display')>-1){
       console.log('displaying');
     }else if (request.url.indexOf('/delete')>-1){
