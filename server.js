5256c977347db52da4fdd23feb8548ae92e503be
Conor C <Conorc1000@gmail.com>
2015-10-06 12:52:08 +0100

    set up server

--- server.js
+++ server.js
@@ -10,9 +10,30 @@ function handler(request, response) {
     //display 'HELLO WORLD' when the user is on the home page
     var url = request.url; //e.g. '/'
     if (url.length === 1) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.end(index);
-  }
-}
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.end(index);
+    }else if(request.url.indexOf('/put')>-1){
+      console.log('put');
+    }else if(request.url.indexOf('/display')>-1){
+      console.log('displaying');
+    }else if (request.url.indexOf('/delete')>-1){
+      console.log('delete');
+    }else{
+      fs.readFile(__dirname + request.url, function(err,file){
+        if (err){
+          response.writeHead(404,{
+            'Content-Type':'text/'+ext
+          });
+        }else{
+          var ext = request.url.split('.')[1];
+          response.writeHead(200,{
+            'Content-Type':'text/'+ext
+          });
+          response.end(file);
+        }
+      });
+    }
+
 
 http.createServer(handler).listen(port);
+}
